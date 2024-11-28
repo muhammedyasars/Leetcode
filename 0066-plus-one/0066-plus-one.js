9832c397da4bad80751ab9ddb313e4d6c7095dd3
Muhammed Yasar S <muhammedyasars810@gmail.com>
2024-11-28 02:30:59 -0200

    Time: 0 ms (100%), Space: 49.2 MB (23.35%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne(digits) {
+    let n = digits.length;
+
+    for (let i = n - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits; 
+        }
+        digits[i] = 0; 
+    }
+
+    digits.unshift(1);
+    return digits;
+}
