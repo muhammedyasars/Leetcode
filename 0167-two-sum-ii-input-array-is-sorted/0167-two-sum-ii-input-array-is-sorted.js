1d54a6418b64510d58303e4c57893898a206456b
Muhammed Yasar S <muhammedyasars810@gmail.com>
2024-12-14 02:37:25 -0200

    Time: 1 ms (92.25%), Space: 50.8 MB (9.09%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -6,9 +6,9 @@
 var twoSum = function(numbers, target) {
         const map = new Map(); 
     for (let i = 0; i < numbers.length; i++) {
-        const complement = target - numbers[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i+1]; 
+        const a = target - numbers[i];
+        if (map.has(a)) {
+            return [map.get(a), i+1]; 
         }
         map.set(numbers[i], i+1); 
     }
