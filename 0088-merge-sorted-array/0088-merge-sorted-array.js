d337b00715e171168e47417d92a5791963153ceb
Muhammed Yasar S <muhammedyasars810@gmail.com>
2024-12-27 07:24:56 -0200

    Time: 0 ms (100%), Space: 48.8 MB (87.01%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+        let result = nums1.slice(0, m).concat(nums2);
+
+    result.sort((a, b) => a - b);
+
+    for (let i = 0; i < result.length; i++) {
+        nums1[i] = result[i];
+    }
+};
