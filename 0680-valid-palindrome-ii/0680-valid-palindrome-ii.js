65a769acbb4e6e3c3ee49d420b6b35fd362be055
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-02-21 09:53:20 +0530

    Time: 6 ms (84.15%), Space: 60.9 MB (8.32%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+   function isPalindrome(str, left, right) {
+        while (left < right) {
+            if (str[left] !== str[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        }
+        left++;
+        right--;
+    }
+    
+    return true; 
+
+    
+};
