a51e25d01cc87a41c934558f42eef91fee595375
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-02-24 16:13:54 +0530

    Time: 2 ms (88.57%), Space: 67.2 MB (11.95%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -1,16 +1,15 @@
 var findMaxAverage = function(nums, k) {
     let sum = 0;
     for (let i = 0; i < k; i++) {
-        sum =sum+ nums[i];
+        sum += nums[i];
     }
     
     let maxSum = sum; 
     
     for (let i = k; i < nums.length; i++) {
-        sum =sum + nums[i];
-        sum =sum - nums[i-k]; 
+        sum += nums[i];
+        sum -= nums[i - k]; 
         maxSum = Math.max(maxSum, sum); 
-        console.log(maxSum,sum)
     }
 
     return maxSum / k; 
