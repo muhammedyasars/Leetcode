2092126be4f60bc3078242e1c6ab661062115601
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-03-01 09:51:15 +0530

    Time: 30 ms (80.7%), Space: 59 MB (18.62%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -1,3 +1,22 @@
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var firstUniqChar = function(s) {
+//     for (let i = 0; i < s.length; i++) {
+//         let count = 0;
+//         for (let j = 0; j < s.length; j++) {
+//             if (s[i] === s[j]) {
+//                 count++;
+//             }
+//         }
+//         if (count === 1) {
+//             return i
+//         } 
+//     }
+//     return -1
+// };
+
 /**
  * @param {string} s
  * @return {number}
