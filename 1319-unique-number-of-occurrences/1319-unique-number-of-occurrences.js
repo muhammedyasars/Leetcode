6f1149b02e833539402ebab682c126ce37675dae
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-03-09 12:41:44 +0530

    Time: 1 ms (79.13%), Space: 53.5 MB (23.78%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
@@ -1,15 +1,12 @@
 var uniqueOccurrences = function(arr) {
     let freqMap = new Map();
 
-    // Count occurrences of each number
     for (let num of arr) {
         freqMap.set(num, (freqMap.get(num) || 0) + 1);
     }
 
-    // Get all frequencies
     let occurrences = [...freqMap.values()];
 
-    // Check if occurrences are unique
     let uniqueSet = new Set(occurrences);
 
     return occurrences.length === uniqueSet.size;
