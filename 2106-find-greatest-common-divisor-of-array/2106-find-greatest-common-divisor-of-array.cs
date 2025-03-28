0e81fd621835e1da8b0fd0bd855d65c847f1386e
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-03-28 15:57:41 +0530

    Time: 5 ms (47.41%), Space: 43.9 MB (7.41%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cs
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cs
@@ -3,13 +3,12 @@ using System.Linq;
 
 public class Solution {
     public int FindGCD(int[] nums) {
-        int maxNum = nums.Max();  // Get max value from array
-        int minNum = nums.Min();  // Get min value from array
+        int maxNum = nums.Max(); 
+        int minNum = nums.Min();
 
-        return GCD(maxNum, minNum); // Compute GCD of max and min
+        return GCD(maxNum, minNum); 
     }
 
-    // Helper function to compute GCD using the Euclidean algorithm
     private int GCD(int a, int b) {
         while (b != 0) {
             int temp = b;
